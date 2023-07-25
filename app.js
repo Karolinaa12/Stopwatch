5737088e7a61d915883cf45c8401981d8f64f1ff
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-07-25 16:16:25 +0200

    Created time display functionality

---
+++ app.js
@@ -0,0 +1,27 @@
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timer = document.querySelector(".time-display");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(displayTimer, 10);
+});
+
+function displayTimer() {
+  milliseconds += 10;
+  milliseconds = milliseconds == 1000 ? 0 : milliseconds;
+  seconds = milliseconds == 1000 ? (seconds + 1) % 60 : seconds;
+  minutes = seconds == 0 && milliseconds == 0 ? (minutes + 1) % 60 : minutes;
+  hours =
+    minutes == 0 && seconds == 0 && milliseconds == 0
+      ? (hours + 1) % 60
+      : hours;
+  let ms = String(milliseconds).padStart(2, "0");
+  let s = String(seconds).padStart(2, "0");
+  let m = String(minutes).padStart(2, "0");
+  let h = String(hours).padStart(3, "0");
+
+  timer.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
